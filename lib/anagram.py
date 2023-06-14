b09ef8a599ec6452dd261b36fdff59baeae1f4c2
michoel <michoelr111@gmail.com>
2023-06-14 16:54:18 -0400

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, arr):
+        lis = []
+        for w in arr:
+            array = list(w)
+            array2 = list(self.word)
+            if sorted(array) == sorted(array2):
+                lis.append(''.join(array))
+        return lis
